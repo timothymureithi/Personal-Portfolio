c2b207ca7ffcd4e8c67f24bd0b3a16be451ad03a
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-11-14 21:48:16 +0300

    header container

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+    <header>
+        <div className='headerContainer flexSB'>
+            <div className='logo'>
+                <img src='./images/logo.png' alt=''/>
+            </div>
+        </div>
+    </header>
+    </>
+
+    )
+}
+
+export default Header
