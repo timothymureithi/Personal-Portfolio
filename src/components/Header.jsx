41a2142ae084545fbbee895676e3042ff227ac7a
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-11-15 10:23:46 +0300

    Navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,6 +8,17 @@ const Header = () => {
             <div className='logo'>
                 <img src='./images/logo.png' alt=''/>
             </div>
+            <nav>
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/">About</a></li>
+                    <li><a href="/">Portfolio</a></li>
+                    <li><a href="/">News</a></li>
+                    <li><a href="/">Contact</a></li>
+
+
+                </ul>
+            </nav>
         </div>
     </header>
     </>
